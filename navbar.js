3519ae61421e0b197cc0cc418ceffbf5709d14be
Kevin McCurdy <khmccurdy@aol.com>
2018-06-11 18:33:18 -0700

    Modify navbar a little

--- navbar.js
+++ navbar.js
@@ -10,18 +10,17 @@ var iconScale = {
     animals: .5,
 }
 
-var navHeight = 100;
 var iconSize = 50;
 var trDuration = 200;
 
-// Create navbar svg element
-var $svg = d3.select("#navbar")
-    .append("svg")
-    .attr("width","100%")
-    .attr("height",navHeight)
+// Select navbar svg element
+var $svg = d3.select("#navbar").select("svg");
 
 var navWidth = parseInt($svg.style("width"));
-var $navElements = $svg.append("g").attr("id","nav-buttons");
+var navHeight = parseInt($svg.style("height"));
+
+var $topButtons = $svg.select("#top-buttons");
+var $subButtons = $svg.select("#sub-buttons");
 
 // Load top buttons
 loadMainButtons();
@@ -31,16 +30,20 @@ function loadMainButtons(){
     // i used to calculate x positions
     var i = 0;
     // Define y position
-    let yPos = navHeight/2;
+    let yMid = navHeight/2;
+    let yTop = navHeight/3;
+    let yLow = 2*navHeight/3;
+
+    $topButtons.attr("transform", `translate(0 ${yMid})`)
     // Loop through top-level menu items
     for (var option in menuStructure){
         // calculate x position so that icons are evenly spaced
         // @Volatile - menu Object may be unordered
         let xPos = navWidth/(totalIcons+1)*(i+1);
         // main button element
-        var $button = $navElements.append("g")
+        var $button = $topButtons.append("g")
             .attr("id", `${option}-button`)
-            .attr("transform", `translate(${xPos} ${yPos})`)
+            .attr("transform", `translate(${xPos} 0)`)
             .attr("fill", "grey");
 
         // Dummy variables to store default transform and color options
@@ -58,7 +61,12 @@ function loadMainButtons(){
             // .attr("href", `icons/${option}.svg`)
             .attr("transform", `translate(${-iconSize/2} ${-iconSize/2}) scale(${scale})`)
 
-        $button.on("click", ()=>{})
+        $button.on("click", function() {
+            // if (typeof menuStructure[option]=="object"){
+            //     $topButtons.transition().duration(trDuration)
+            //         .attr("transform", `translate(0 ${yTop})`);
+            // }
+        })
 
         // Mouseover (scale up slightly, change color)
         $button.on("mouseover", function(){
