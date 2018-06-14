90c4abe7010b26898229f01b1845013da7c32b0b
Kevin McCurdy <khmccurdy@aol.com>
2018-06-13 20:11:25 -0700

    And here's a 3d-ish thing

--- scatter.js
+++ scatter.js
@@ -29,6 +29,10 @@ d3.csv("data_cleanup/deforestation_cleanup.csv", (error, response)=>{
     var trace3 = {
         x: deforestation90s,
         y: deforestation00s,
+        marker:{
+            color: totalSpecies,
+            colorscale: [['0','#eeb'],['1','#300']]
+        },
         mode:'markers'
     }
 
@@ -42,12 +46,37 @@ d3.csv("data_cleanup/deforestation_cleanup.csv", (error, response)=>{
         
         mode:'markers'
     }
-    var data = [trace4];
+    var data = [trace3];
 
     var layout = {};
 
     Plotly.newPlot('scatterplot',data,layout)
     // Plotly.animate('scatterplot',{data:[trace1]})
-
     console.log(protectedLand14);
+
+    var trace5 = {
+        x: protectedLand90,
+        y: deforestation90s,
+        z: totalSpecies,
+        marker:{
+            size: 5,
+            opacity: 0.5,
+        },
+        type: 'scatter3d',
+        mode: 'markers',
+    }
+    var trace6 = {
+        x: protectedLand14,
+        y: deforestation00s,
+        z: totalSpecies,
+        type: 'scatter3d',
+        marker: trace5.marker,
+        mode: 'markers',
+    }
+
+    var data3d = [trace5, trace6];
+    var layout3d = {};
+
+    Plotly.newPlot('3d',data3d,layout3d);
+    // Plotly.animate('3d',{data:[trace6]},);
 })
