9caaf9c2744dc8b3010741d2bec5d17e92fbade2
Kevin McCurdy <khmccurdy@aol.com>
2018-06-14 22:23:09 -0700

    split out heatmap.js + minor adjustments

---
+++ heatmap.js
@@ -0,0 +1,30 @@
+var mapbox = `https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=${mapboxToken}`
+var heatMultiplier = 3;
+
+var myMap = L.map('map', {
+    // center: [37.7749, -122.4194],
+    center:[0,0],
+    zoom: 3
+});
+
+L.tileLayer(mapbox).addTo(myMap);
+
+
+d3.csv("/threatened_species.csv", function (data) {
+
+    var heatArray = data.map(d => d["Coordinates"].split(",").map(d => +d));
+    var latitudeArray = heatArray.map(d => d[0]);
+    var longitudeArray = heatArray.map(d => d[1]);
+
+    data.forEach(function (d,i) {
+        heatArray[i].push(+d.Total*heatMultiplier)
+    });
+
+    console.log(data[0]);
+
+    var heat = L.heatLayer(heatArray, {
+            radius: 15,
+            blur: 15
+        }
+    ).addTo(myMap)
+});
