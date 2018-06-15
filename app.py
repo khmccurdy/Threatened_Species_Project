1da7cead8eb1f8161866cc29310dab7e5a3b969d
Swati Giri <swatigiri3392@gmail.com>
2018-06-14 17:52:16 -0700

    threatened species fix

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def get_otu_ids():
     # Final data
     data = res
     #getting the data
-    return jsonify(data)
+    return jsonify(data[0 :])
 
 if __name__ == "__main__":
     app.run(debug=True)  
